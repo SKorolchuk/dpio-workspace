d2e031126f7fc895590b58fb735a32dfb5d0f080
Serge Korolchuk <sergey.korolchuk.dev@gmail.com>
2020-07-29 00:57:30 +0300

    #1 Setup initial project structure - done (#7)

---
+++ Deeproxio.Workspace.API/Configuration/ServiceRegistrationExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using Deeproxio.Persistence.Identity.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Deeproxio.UserManagement.API.Extensions
+{
+    public static class ServiceRegistrationExtensions
+    {
+        public static void AddJWTAuthorization(this IServiceCollection services, IConfiguration configuration)
+        {
+            string secretKey = configuration.GetValue("JwtIssuerOptions:SecretKey", "");
+
+            if (string.IsNullOrWhiteSpace(secretKey))
+            {
+                throw new ArgumentNullException(secretKey);
+            }
+
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey));
+
+            var jwtAppSettingOptions = configuration.GetSection(nameof(JwtIssuerOptions));
+
+            // Configure JwtIssuerOptions
+            services.Configure<JwtIssuerOptions>(options =>
+            {
+                options.Issuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                options.Audience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)];
+                options.SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+            });
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)],
+
+                ValidateAudience = false,
+                ValidAudience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)],
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+
+                RequireExpirationTime = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.ClaimsIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                    options.TokenValidationParameters = tokenValidationParameters;
+                    options.SaveToken = true;
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("JWT Claims", policy => policy.RequireClaim(
+                    Constants.Strings.JwtClaimIdentifiers.Rol,
+                    Constants.Strings.JwtClaims.ApiAccess));
+            });
+        }
+    }
+}
---
+++ Deeproxio.Workspace.BLL.Contract/Entities/Project.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deeproxio.Workspace.BLL.Contract.Entities
+{
+    class Project
+    {
+    }
+}
